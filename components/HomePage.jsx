3cf68e21b8a5b030b3207a105b7f2c54f7b6916d
HaydarChan <atqiyahaydar15@gmail.com>
2025-06-02 03:32:46 +0700

    feat: add particle backgrounds

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
+import Gif from '@/public/gif3.gif'
+import Particles from './Particles';
 
 const HomePage = () => {
   return (
-    <div className='h-full w-full flex flex-col items-center justify-center bg-white'>
-
+    <div className='h-full w-full flex flex-col bg-white p-4 relative'>      
+      <Particles
+        particleColors={['#000000', '#000000']}
+        particleCount={200}
+        particleSpread={10}
+        speed={0.1}
+        particleBaseSize={100}
+        moveParticlesOnHover={true}
+        alphaParticles={false}
+        disableRotation={false}
+      />
     </div>
   )
 }
